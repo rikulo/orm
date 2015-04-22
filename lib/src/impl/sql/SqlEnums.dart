4ac2eceea66303c0644deee4fad13f183f2e1b96
luis <luisbigvargas@gmail.com>
2015-04-21 23:08:50 -0400

    convert from const Enum to enum

--- lib/src/impl/sql/SqlEnums.dart
+++ lib/src/impl/sql/SqlEnums.dart
@@ -2,25 +2,19 @@ part of rikulo_orm_impl;
 
 
 /** Used with SqlToken.TABLE_DEF */
-class ScopeEnum extends Enum {
-  static const ScopeEnum GLOBAL = const ScopeEnum("GLOBAL");
-  static const ScopeEnum LOCAL = const ScopeEnum("LOCAL");
-
-  const ScopeEnum(String name) : super(name);
+enum ScopeEnum {
+  GLOBAL,
+  LOCAL
 }
 
 /** Used with SqlToken.TABLE_DEF */
-class CommitEnum extends Enum {
-  static const CommitEnum DELETE = const CommitEnum("DELETE");
-  static const CommitEnum PRESERVE = const CommitEnum("PRESERVE");
-
-  const CommitEnum(String name) : super(name);
+enum CommitEnum {
+  DELETE,
+  PRESERVE
 }
 
 /** Used with SqlToken.UNIQUE_CONSTRAINT_DEF */
-class UniqueSpecEnum extends Enum {
-  static const UniqueSpecEnum UNIQUE = const UniqueSpecEnum("UNIQUE");
-  static const UniqueSpecEnum PRIMARY_KEY = const UniqueSpecEnum("PRIMARY KEY");
-
-  const UniqueSpecEnum(String name) : super(name);
+enum UniqueSpecEnum {
+  UNIQUE,
+  PRIMARY_KEY
 }
